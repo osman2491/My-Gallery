4c777b451c37408562f27b807c6dc88bf9aa560c
osman67239121@gmail.com <osman67239121@gmail.com>
2019-12-14 22:40:08 +0300

    adds category and location models to the database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class category(models.Model):
+    categorys = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.categorys
+
+class image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_discription = models.CharField(max_length=30)
+    Location = models.ForeignKey(Location)
+    category = models.ForeignKey(category)
+    
+
+    def __str__(self):
+        return self.image_name
+    
+
+
+
+
