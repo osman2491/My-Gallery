09dc5d0b7ef65e70b84ff2b2c3b050ab2d1defe6
osman67239121@gmail.com <osman67239121@gmail.com>
2019-12-14 23:02:32 +0300

    test setup method

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Location,category,image
 
-# Create your tests here.
+class imageTestClass(TestCase):
+
+    def setUp(self):
+        self.images= image(image_name = 'usa',image_discription='usa is a great country')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.images,image))
+
+
+
