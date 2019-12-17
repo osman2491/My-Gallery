1535eb17cb9d40a663e4dee846d91997248c1365
osman67239121@gmail.com <osman67239121@gmail.com>
2019-12-17 08:10:50 +0300

    test get image by id method

--- app/tests.py
+++ app/tests.py
@@ -73,5 +73,11 @@ class TestImage(TestCase):
         changed_img = Image.objects.filter(image='photos/ali.jpg')
         self.assertTrue(len(changed_img) > 0)
 
+    def test_get_image_by_id(self):
+        found_image = self.image_test.get_image_by_id(self.image_test.id)
+        image = Image.objects.filter(id=self.image_test.id)
+        self.assertTrue(found_image, image)
+
+
 
 
