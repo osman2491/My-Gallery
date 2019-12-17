66defc59935e73cba97774b958eefb4ece9adc25
osman67239121@gmail.com <osman67239121@gmail.com>
2019-12-17 10:35:08 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'
 STATIC_URL = '/static/'
