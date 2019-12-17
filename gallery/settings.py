a59a100ddc32897ad8d92da01f261128245d6e8c
osman67239121@gmail.com <osman67239121@gmail.com>
2019-12-17 09:49:05 +0300

    removed base dir

--- gallery/settings.py
+++ gallery/settings.py
@@ -52,7 +52,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '2491'
+SECRET_KEY='2491'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -154,6 +154,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'
 STATIC_URL = '/static/'
