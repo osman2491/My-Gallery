d8887953d38d4bab261c5747440d32468df9d52e
osman67239121@gmail.com <osman67239121@gmail.com>
2019-12-17 09:36:46 +0300

    removed base dir

--- gallery/settings.py
+++ gallery/settings.py
@@ -52,7 +52,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2491'
+SECRET_KEY='2491'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
